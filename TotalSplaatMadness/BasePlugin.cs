75a0562d0111fd2a37f0b66bf060a860b577f218
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2026-03-10 12:56:19 -0300

    Add project files.

---
+++ TotalSplaatMadness/BasePlugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+using MTM101BaldAPI;
+
+namespace TotalSplaatMadness
+{
+    [BepInPlugin("sticky.bbplus.splaatvariants", "Total Splaat Madness", "0.0.1")]
+
+    [BepInDependency("mtm101.rulerp.bbplus.baldidevapi")]
+
+    public class BasePlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new Harmony("sticky.bbplus.splaatvariants");
+
+            harmony.PatchAll();
+        }
+    }
+}
